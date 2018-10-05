5d116125dace7e02c7ac8d333f2bbc13c9c7881d
Kristian-A <kristian.zh.apostolov@gmail.com>
2018-10-05 22:58:21 +0300

    Added almost working corner

--- sketch/center.js
+++ sketch/center.js
@@ -1,9 +1,9 @@
 class Center {
 
-	constructor(color, pos) {
-		this.color = color;
-		this.pos = pos;
-		this.connections = undefined;
+	constructor(col, pos) {
+		this.col = col;
+		this.pos = createVector(pos[0], pos[1], pos[2]);
+		this.connections = [];
 	}
 
 	connect(top, right, bottom, left) {
@@ -11,13 +11,50 @@ class Center {
 	}
 
 	draw() {
+		let m = createVector(this.pos.x, this.pos.y, this.pos.z).mult(SIDE_LENGTH);
 		push();
-		translate(this.pos[0] * SIDE_LENGTH, this.pos[1] * SIDE_LENGTH, this.pos[2] * SIDE_LENGTH);
-		fill(this.color);
+		fill(this.col);
 		stroke(0);
 		strokeWeight(2);
+		translate(m.x, m.y, m.z);
 		box(SIDE_LENGTH);
 		pop();
 	}
 
 };
+
+class Edge {
+
+	constructor(c1, c2) {
+		this.pos1 = p5.Vector.add(c1.pos, c2.pos).add(c1.pos.copy().mult(0.5))
+		this.pos1.mult(SIDE_LENGTH);
+		this.rot1 = c1.pos.y ? HALF_PI : 0;
+		this.col1 = c1.col;
+
+		this.pos2 = p5.Vector.add(c1.pos, c2.pos).add(c2.pos.copy().mult(0.5))
+		this.pos2.mult(SIDE_LENGTH);
+		this.rot2 = c2.pos.y ? HALF_PI : 0;
+		this.col2 = c2.col;
+	}
+
+	draw() {
+		push();
+		stroke(0);
+		strokeWeight(2);
+		fill(this.col1);
+		translate(this.pos1.x, this.pos1.y, this.pos1.z);
+		rotateX(this.rot1);
+		plane(SIDE_LENGTH, SIDE_LENGTH);
+		pop();
+
+		push();
+		stroke(0);
+		strokeWeight(2);
+		fill(this.col2);
+		translate(this.pos2.x, this.pos2.y, this.pos2.z);
+		rotateX(this.rot2);
+		plane(SIDE_LENGTH, SIDE_LENGTH);
+		pop();
+	}
+
+}
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,8 +1,8 @@
 const SIDE_LENGTH = 100
 
-let centers = [];
-
-angle = 0.1;
+let centers = {};
+let edges = {};
+let angle = 0;
 
 function setup() {
 	createCanvas(600, 600, WEBGL);
@@ -15,20 +15,28 @@ function setup() {
 		g: new Center([0, 255, 0], [0, 0, -1])
 	};
 
+	edges = {
+		yg: new Edge(centers.y, centers.g),
+		yr: new Edge(centers.y, centers.r)
+	};
+
 	centers.y.connect(centers.g, centers.r, centers.b, centers.o);
 	centers.r.connect(centers.y, centers.g, centers.b, centers.b);
 	centers.w.connect(centers.r, centers.g, centers.o, centers.b);
 	centers.o.connect(centers.y, centers.b, centers.w, centers.g);
 	centers.b.connect(centers.y, centers.o, centers.w, centers.r);
-	centers.b.connect(centers.y, centers.r, centers.w, centers.o);
+	centers.g.connect(centers.y, centers.r, centers.w, centers.o);
 }
 
 function draw() {
 	background(51);
-	rotateX(angle);
-	//angle += 0.05;
-
+	rotateX(-QUARTER_PI);
+	rotateY(angle);
 	for (let c of Object.values(centers)) {
 		c.draw();
+	}
+	angle += 0.01
+	for (let e of Object.values(edges)) {
+		e.draw();
 	}
 }
